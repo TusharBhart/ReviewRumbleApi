7af2e8e764813db6fddfd526c1759c8398edc20f
gauravpreet-wg <gauravpreet.singh@watchguard.com>
2024-12-10 01:31:06 +0530

    allow enum as string in body

--- ReviewRumble/Models/UpdatePullRequestStatus.cs
+++ ReviewRumble/Models/UpdatePullRequestStatus.cs
@@ -1,6 +1,9 @@
-﻿namespace ReviewRumble.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace ReviewRumble.Models;
 
 public class UpdatePullRequestStatus
 {
+    [JsonConverter(typeof(JsonStringEnumConverter<ReviewStatusEnum>))]
     public ReviewStatusEnum Status { get; set; }
 }
